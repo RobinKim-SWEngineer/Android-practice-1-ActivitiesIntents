a826c132b3b19b37c7149185f6aeff5588bf15ab
Robin Kim <hgkim@seznam.cz>
2021-04-14 17:50:58 +0200

    Restroing previous activity state is added

--- ScrollingText/app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ ScrollingText/app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.scrollingtext;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,9 +14,15 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String COMMENT_MESSAGE = "com.example.scrollingtext.commentMessage";
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private String textViewFeedbackHeadingVisibilityState;
+    private String textViewFeedbackMessageState;
+
     private EditText editTextComment;
     private TextView textViewFeedbackHeading;
     private TextView textViewFeedback;
+
     private final int FEEDBACK_REQUEST = 1;
 
     @Override
@@ -22,9 +30,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(LOG_TAG, "------");
+        Log.d(LOG_TAG, "onCreate");
+
         textViewFeedback = findViewById(R.id.textView_feedback_message);
         textViewFeedbackHeading = findViewById(R.id.textView_reply_heading);
         editTextComment = findViewById(R.id.editText_comment);
+
+        /* Restoring saved state from last destroyed ( itself ) activity */
+        if (savedInstanceState != null) {
+
+            int previousVisibilityState = savedInstanceState.getInt(textViewFeedbackHeadingVisibilityState);
+            String previousFeedbackMessageState = savedInstanceState.getString(textViewFeedbackMessageState);
+
+            textViewFeedback.setVisibility(previousVisibilityState);
+            textViewFeedback.setText(previousFeedbackMessageState);
+
+            textViewFeedbackHeading.setVisibility(previousVisibilityState);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 
     public void sendComment(View view) {
@@ -53,5 +112,23 @@ public class MainActivity extends AppCompatActivity {
                 textViewFeedbackHeading.setVisibility(View.VISIBLE);
 
             }
+    }
+
+    /**
+     *  System calls this method between onPause() and onStop().
+     *  Use this method in case you need to save data when Activity is destroyed and recreated.
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if (textViewFeedbackHeading.getVisibility() == View.VISIBLE) {
+            /**
+             *  Saving Activity instance states to restore them upon recreation.
+             *  Their instance state is available in "savedInstanceState" as a Bundle object.
+             */
+            outState.putInt(textViewFeedbackHeadingVisibilityState, View.VISIBLE);
+            outState.putString(textViewFeedbackMessageState, textViewFeedback.getText().toString());
+        }
     }
 }
--- ScrollingText/app/src/main/java/com/example/scrollingtext/SecondActivity.java
+++ ScrollingText/app/src/main/java/com/example/scrollingtext/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,12 +12,16 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     public static final String FEEDBACK_MESSAGE = "com.example.scrollingtext.feedbackMessage";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
     public EditText editTextReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Log.d("LOG_TAG", "------");
+        Log.d(LOG_TAG, "onCreate");
 
         TextView textViewComment = findViewById(R.id.textView_received_comment);
         editTextReply = findViewById(R.id.editText_reply_upon_comment);
@@ -25,6 +30,42 @@ public class SecondActivity extends AppCompatActivity {
 
         String comment = intent.getStringExtra(MainActivity.COMMENT_MESSAGE);
         textViewComment.setText(comment);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 
     public void sendFeedback(View view) {
@@ -36,6 +77,7 @@ public class SecondActivity extends AppCompatActivity {
 
         setResult(RESULT_OK, replyIntent);
 
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 }
